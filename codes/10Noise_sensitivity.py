5db8f9b997161b769d04ff8ee472075d09b1110d
Marc Gillioz <marc.gillioz@gmail.com>
2026-09-03 15:03:21 +0200

    modify x axis labels

--- codes/10Noise_sensitivity.py
+++ codes/10Noise_sensitivity.py
@@ -177,7 +177,7 @@ for model_key, df_model in f2_all.groupby("model"):
 
     fig, ax = plt.subplots(figsize=(8, 3))
     f2_by_noise.plot.box(ax=ax)
-    ax.set(xlabel="Noise std [MW]", ylabel="F₂", ylim=(0.75, 1))
+    ax.set(xlabel="Noise amplitude [MW]", ylabel="F₂", ylim=(0.75, 1))
     fig.tight_layout()
     fig.savefig(
         pjoin(
@@ -198,7 +198,7 @@ for i, (model_key, df_model) in enumerate(f2_all.groupby("model")):
         median_f2.index, median_f2.values, marker="o", color=colors[i], label=model_key
     )
 
-ax.set(xlabel="Noise std [MW]", ylabel="Median F₂", ylim=(0.75, 1))
+ax.set(xlabel="Noise amplitude [MW]", ylabel="Median F₂", ylim=(0.75, 1))
 ax.legend()
 fig.tight_layout()
 fig.savefig(
@@ -222,7 +222,7 @@ if learning == "unsupervised" and not rmse_all.empty:
 
         fig, ax = plt.subplots(figsize=(8, 3))
         rmse_by_noise.plot.box(ax=ax)
-        ax.set(ylim=0, xlabel="Noise std [MW]", ylabel="RMSE [MW]")
+        ax.set(ylim=0, xlabel="Noise amplitude [MW]", ylabel="RMSE [MW]")
         fig.tight_layout()
         fig.savefig(
             pjoin(
@@ -246,7 +246,7 @@ if learning == "unsupervised" and not rmse_all.empty:
             label=model_key,
         )
 
-    ax.set(ylim=0, xlabel="Noise std [MW]", ylabel="Median RMSE [MW]")
+    ax.set(ylim=0, xlabel="Noise amplitude [MW]", ylabel="Median RMSE [MW]")
     ax.legend()
     fig.tight_layout()
     fig.savefig(
